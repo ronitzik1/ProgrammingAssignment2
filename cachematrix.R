d30534647d5e7fcbd557d79306eb5242806f6caa
ronitzik1 <ronitzik1@gmail.com>
2015-12-10 16:46:17 +0200

    submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,76 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are used to create a matrix object that 
+## can cache it's inverse matrix, once it was calculated.
 
-## Write a short comment describing this function
 
+## Create a matrix object that can cache it's inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        
+        ## Set the matrix data.
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## Get the matrix data.
+        get <- function() { x }
+        
+        ## Store the inverse matrix.
+        setinv <- function(inv) {
+                m <<- inv
+        }
+        
+        ## Get the inverse matrix.
+        getinv <- function() { m }
+        
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x'. 
+## If the inverse was already calculated once, than the cached
+## inverse matrix is returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinv()
+        
+        ## If the inverse matrix was never calculated until now.
+        if(is.null(m)) {
+                ## Get the data
+                data <- x$get()
+                ## Find the inverse matrix
+                m <- solve(data, ...)
+                ## Cache the inverse matrix
+                x$setinv(m)
+        } else {
+                message('returning cached data')
+        }
+        
+        return (m)
+}
+
+test <- function() {
+        ## This matrix should almost always be invertible
+        myMatrix <- matrix(rnorm(25), 5, 5)
+        
+        ## Store our inverted matrix
+        myInv <- solve(myMatrix)
+        
+        ## Create our cache matrix
+        cMat <- makeCacheMatrix(myMatrix)
+        
+        ## Get our inverse matrix using the cache matrix.
+        cInv <- cacheSolve(cMat)
+        
+        ## Get our inverse matrix using the cache matrix, this time
+        ## a message should be printed stating that the return value 
+        ## is the cached result.
+        cInv <- cacheSolve(cMat)
+        
+        print('regular inverse matrix')
+        print(myInv)
+        print('cached inverse matrix')
+        print(cInv)
 }
